84a3dbec3122ed5c2d484b0f16f8a541650e58fb
parkhyeseong <2017cococo@gmail.com>
2024-06-24 20:45:44 +0900

    6_24

---
+++ 6_24/map/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import IterationSample from './IterationSample';
+
+function App() {
+  return (
+    <IterationSample></IterationSample>
+  );
+}
+
+export default App;
