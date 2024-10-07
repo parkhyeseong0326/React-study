d6c79f702e635f80c8e2d05be493b78334c68070
parkhyeseong0326 <2017cococo@gmail.com>
2024-10-07 20:17:34 +0900

    10_7

--- shop/src/Routes/Cart.js
+++ shop/src/Routes/Cart.js
@@ -1,8 +1,13 @@
 import {Table} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName } from '../store';
 
 function Cart() {
+    let state = useSelector((state) => state)
+    let dispatch = useDispatch()
     return (
         <div>
+            {state.user}의 장바구니
             <Table>
                 <thead>
                     <tr>
@@ -13,12 +18,21 @@ function Cart() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>안녕</td>
-                        <td>하세</td>
-                        <td>요</td>
-                    </tr>
+                    {
+                        state.cart.map((a,i) => 
+                        <tr ket = {i}>
+                            <td>1</td>
+                            <td>{state.cart[i].name}</td>
+                            <td>{state.cart[i].count}</td>
+                            <td>
+                                <button onClick={() => {
+                                    dispatch(changeName())
+                                }}>+</button>
+                            </td>
+                        </tr>
+                        )
+                    }
+                    
                 </tbody>
             </Table>
         </div>
--- shop/src/store.js
+++ shop/src/store.js
@@ -1,5 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore , createSlice} from "@reduxjs/toolkit";
+
+let user = createSlice({
+    name : 'user',
+    initialState : 'kim',
+    reducers : {
+        changeName(state){
+            return 'john' + state
+        }
+    }
+})
+export let {changeName} = user.actions
+
+let stock = createSlice({
+    name : 'stock',
+    initialState : [10,11,12]
+})
+
+let cart = createSlice({
+    name : 'cart',
+    initialState : [
+        {id : 0, name : 'white and black', count : 2},
+        {id : 2, name : 'Grey Yordan', count : 1}
+    ]
+})
 
 export default configureStore({
-    reducer : { }
+    reducer : { 
+        user : user.reducer,
+        stock : stock.reducer,
+        cart : cart.reducer
+    }
 })
